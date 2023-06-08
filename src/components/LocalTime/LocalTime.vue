2d19ade193fb03dd882f332d318c20e66de364e0
Helen-Z-storage <helenamanbar21534@gmail.com>
2023-06-07 20:31:34 -0400

    fix local time
    success on display current location time

--- src/components/LocalTime/LocalTime.vue
+++ src/components/LocalTime/LocalTime.vue
@@ -15,15 +15,35 @@ const props = defineProps<{
 const currDate = ref(new Date())
 const timeZoneOffset = ref(new Date().getTimezoneOffset())
 const timeZoneName = ref("")
+const utcTimestampInSec = computed(
+  () => Math.floor((currDate.value.getTime() + currDate.value.getTimezoneOffset()) / 1000))
 
-const utcTimestampInSec = computed(() => Math.floor(currDate.value.getTime() + currDate.value.getTimezoneOffset() / 1000))
-const lat = computed(props.lastMarker.lat())
-const lng = computed(props.lastMarker.lng())
-const timeZoneResponse = await fetchTimeZoneByLocationAndTimestamp(lat, lng, utcTimestampInSec)
-const { dstOffset, rawOffset, timeZoneName } = timeZoneResponse
-timeZoneOffset.value = (dstOffset + rawOffset) * 1000
-timeZoneName.value = timeZoneName
 const localTime = computed(() => new Date(new Date().getTime() + timeZoneOffset.value).toLocaleString())
+watch(() => props.lastMarker, async () => {
+  return;
+  if (!props.lastMarker) return;
+  console.log(props.lastMarker)
+  const { lat, lng } = props.lastMarker.position
+  console.log(lat, lng)
+  const timeZoneResponse = await fetchTimeZoneByLocationAndTimestamp(lat(), lng(), utcTimestampInSec.value)
+  const { dstOffset, rawOffset, timeZoneName: timeZoneName1 } = timeZoneResponse
+  timeZoneOffset.value = (dstOffset + rawOffset) * 1000
+  timeZoneName.value = timeZoneName1
+  console.log(timeZoneResponse)
+})
+
+// const currDate = ref(new Date())
+// const timeZoneOffset = ref(new Date().getTimezoneOffset())
+// const timeZoneName = ref("")
+
+// const utcTimestampInSec = computed(() => Math.floor(currDate.value.getTime() + currDate.value.getTimezoneOffset() / 1000))
+// const lat = computed(props.lastMarker.lat())
+// const lng = computed(props.lastMarker.lng())
+// const timeZoneResponse = await fetchTimeZoneByLocationAndTimestamp(lat, lng, utcTimestampInSec)
+// const { dstOffset, rawOffset, timeZoneName: timeZoneName1  } = timeZoneResponse
+// timeZoneOffset.value = (dstOffset + rawOffset) * 1000
+// timeZoneName.value = timeZoneName1
+// const localTime = computed(() => new Date(new Date().getTime() + timeZoneOffset.value).toLocaleString())
 
 </script>
 
@@ -32,11 +52,11 @@ const localTime = computed(() => new Date(new Date().getTime() + timeZoneOffset.
   position: absolute;
   right: 12px;
   top: 12px;
-
+  z-index: 10000;
   width: 200px;
   padding: 12px;
 
-  background: rgba(0, 0, 0, .3);
+  background: rgb(0, 0, 0);
   color: white;
 }
 </style>
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const timeBaseURL = "https://maps.googleapis.com/maps/api/timezone/json"
+export const API_KEY = "AIzaSyCLkxXnD4DzuyUZgp0gMJDvIgFOiXZ7ZVE"
