24cf604765c47ad1475581b3a6bae947fca594e3
Helen-Z-storage <helenamanbar21534@gmail.com>
2023-06-07 21:16:26 -0400

    Update LocalTime.vue
    - fixed display local time's calculation
    - init local time and local time zone name with current location
    - update local time per sec

--- src/components/LocalTime/LocalTime.vue
+++ src/components/LocalTime/LocalTime.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="localTime">
     <div>{{ localTime }}</div>
-    <div>{{ timeZoneName }}</div>
+    <div>{{ localTimeZoneName }}</div>
   </div>
 </template>
 <script setup lang="ts">
@@ -13,22 +13,53 @@ const props = defineProps<{
 }>()
 
 const currDate = ref(new Date())
-const timeZoneOffset = ref(new Date().getTimezoneOffset())
-const timeZoneName = ref("")
-const utcTimestampInSec = computed(
-  () => Math.floor((currDate.value.getTime() + currDate.value.getTimezoneOffset()) / 1000))
+const localTimeZoneName = ref("")
 
-const localTime = computed(() => new Date(new Date().getTime() + timeZoneOffset.value).toLocaleString())
+// getTime return timestamp in milisec, getTimezoneOffset() return offset in min
+const utcTimestampInMiliSec = computed(
+  () => currDate.value.getTime() + currDate.value.getTimezoneOffset() * 60 * 1000)
+const utcTimestampInSec = computed(
+  () => Math.floor(utcTimestampInMiliSec.value / 1000))
+
+// store offset that utcTimestamp + offset = local timestamp
+const timeZoneOffsetInSec = ref( - new Date().getTimezoneOffset() * 60)
+const localTime = computed(() => new Date(utcTimestampInMiliSec.value + timeZoneOffsetInSec.value * 1000).toLocaleString())
+
+onMounted(() => {
+  
+  // Try HTML5 geolocation, setting curr.location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        const { lat, lng } = pos;
+        const timeZoneResponse = await fetchTimeZoneByLocationAndTimestamp(lat, lng, utcTimestampInSec.value)
+        const { timeZoneName } = timeZoneResponse
+        localTimeZoneName.value = timeZoneName
+      },
+      () => console.warn("Error: The Geolocation service failed")
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    console.warn("Browser doesn't support Geolocation")
+    return
+  }
+  // for time jumping in sec
+  setInterval(() => { currDate.value = new Date() }, 1000);
+})
+
 watch(() => props.lastMarker, async () => {
-  return;
-  if (!props.lastMarker) return;
+  if (!props.lastMarker || !props.lastMarker.position) return;
   console.log(props.lastMarker)
   const { lat, lng } = props.lastMarker.position
   console.log(lat, lng)
   const timeZoneResponse = await fetchTimeZoneByLocationAndTimestamp(lat(), lng(), utcTimestampInSec.value)
-  const { dstOffset, rawOffset, timeZoneName: timeZoneName1 } = timeZoneResponse
-  timeZoneOffset.value = (dstOffset + rawOffset) * 1000
-  timeZoneName.value = timeZoneName1
+  const { dstOffset, rawOffset, timeZoneName } = timeZoneResponse
+  timeZoneOffsetInSec.value = (dstOffset + rawOffset) * 1000
+  localTimeZoneName.value = timeZoneName
   console.log(timeZoneResponse)
 })
 
